6c3708725c205616dcd4d8aaba1efc4c8551b83a
qasd10651 <qasd10651@gmail.com>
2026-08-29 20:35:35 +0800

    Add files via upload

---
+++ externs.js
@@ -0,0 +1,15 @@
+/** @externs */
+var require;
+var exports;
+var getSerial;
+
+Object.prototype.emit;
+Object.prototype.on;
+Object.prototype.write;
+Object.prototype.end;
+
+Object.prototype.connect;
+Object.prototype.subscribe;
+Object.prototype.publish;
+Object.prototype.disconnect;
+Object.prototype.create;
